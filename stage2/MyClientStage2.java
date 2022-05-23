e491812f888d5db88142a812ee0761d3d78edc11
GitHubJono <jonathan.crossley@students.mq.edu.au>
2022-05-23 16:17:28 +1000

    working stage2 file

--- stage2/MyClientStage2.java
+++ stage2/MyClientStage2.java
@@ -25,51 +25,65 @@
     
     //Gets first job
     String job = (String)bin.readLine();  
-    String jobPars = (String)bin.readLine(); 
+    String jobPars[] = job.split(" ");
+    String selectedServer = "";
+    String checkServer = "";
+    String[] selectedServerPars;
+    String serverName = "";
+    String serverNumber = "";
+    
+    dout.write(("OK\n").getBytes());
 
+    str = (String)bin.readLine(); 
     str = (String)bin.readLine(); 
     Integer doJobs = 0; 
     
+    
     //Loops through necessary requests
     while(!jobPars[0].equals("NONE")) {
+    
+    	System.out.println("Ready for next object");
     	
     	dout.write(("REDY\n").getBytes());
     	
     	job = (String)bin.readLine();  
      	jobPars = job.split(" ");
+     	
     	
     	//If there is a job, schedule it
     	if (jobPars[0].equals("JOBN")) {
     	
-    		String bestServerParameters = jobPars[4]+" "+jobPars[5]+" "+jobPars[6]
+    		//str = (String)bin.readLine(); 
+    	
+    		String bestServerParameters = jobPars[4]+" "+jobPars[5]+" "+jobPars[6];
+    		
+    		System.out.println("Finding available servers...");
     		
     		dout.write(("GETS Avail "+bestServerParameters+"\n").getBytes());
     		
     		//splits server information into relevant parameters
     		String serverData = (String)bin.readLine();
+    		
+    		System.out.println(serverData);
+    		
     		String[] serverDataPars = serverData.split(" ");
     		Integer totalServers = Integer.parseInt(serverDataPars[1]);
     		
+    		System.out.println("Sending OK 1");
+    		
     		dout.write(("OK\n").getBytes());
     		
-    		String selectedServer = "";
     		
-    		for (int i = 0; i < totalServers; i++) {
-    			checkServer = bin.readLine();
-    			if (i == 0) {
-    				selectedServer = checkServer;
-    			}
-    		}
-    		
-    		String[] selectedServerPars = selectedServer.split(" ");
-    		String serverName = selectedServerPars[0];
-    		String serverNumber = selectedServerPars[1];
     		
     		if (totalServers == 0) {
+    		
+    			System.out.println("Ready for next object 2");	
     		
     			str = (String)bin.readLine(); 
     			dout.write(("REDY\n").getBytes());
     			job = (String)bin.readLine();  
+    			
+    			System.out.println("Finding capable servers...");
     			
     			dout.write(("GETS Capable "+bestServerParameters+"\n").getBytes());
     		
@@ -77,10 +91,13 @@
     			serverData = (String)bin.readLine();
     			serverDataPars = serverData.split(" ");
     			totalServers = Integer.parseInt(serverDataPars[1]);
+    			
+    			System.out.println("Sending OK 2");
     		
     			dout.write(("OK\n").getBytes());
     		
     			selectedServer = "";
+    			checkServer = "";
     		
     			for (int i = 0; i < totalServers; i++) {
     				checkServer = bin.readLine();
@@ -94,6 +111,29 @@
     			serverNumber = selectedServerPars[1];
     			 
     		}
+    		else
+    		{ 
+			selectedServer = "";
+    			checkServer = "";
+    		
+    			for (int i = 0; i < totalServers; i++) {
+    				checkServer = bin.readLine();
+    				if (i == 0) {
+    					selectedServer = checkServer;
+    				}
+    			}
+    		
+    			selectedServerPars = selectedServer.split(" ");
+    			serverName = selectedServerPars[0];
+    			serverNumber = selectedServerPars[1];
+		}
+    		
+    		System.out.println("Sending OK 3");
+    		
+    		dout.write(("OK\n").getBytes());
+    		str = (String)bin.readLine();
+    		
+    		System.out.println("Scheduling job..."); 
     	
     		dout.write(("SCHD "+doJobs+" "+serverName+" "+serverNumber+"\n").getBytes());
     		doJobs++;
@@ -102,6 +142,8 @@
     	else
     	{
     		//otherwise continue on with the next request
+    		System.out.println("Sending OK 4");
+    		
     		dout.write(("OK\n").getBytes());
     		str = (String)bin.readLine();  
     	}
